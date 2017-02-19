4ce16209d34c2af63f520c22db7e91811e897f1a
Daniel Mar <mardaniel42@gmail.com>
2017-02-20 11:23:57 +1300

    added comments

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
+//variable that holds the positions of the players and enemies
 var playerPositions = [];
 var enemyPositions = [];
+//variable that holds all the player pieces that are "valid" - have valid moves. This is used for highlighting
 var validPlayerPieces = [];
+
+//If there are no mandatory moves, then any move is legal.
 var anyMoveIsLegal = true;
 
+//function that adds pieces to the board
 function addPieces() {
+  //gets all the playing boxes/squares and puts them in an array
   var innerBoxes = document.getElementsByClassName('innerBoxes');
 
-  var playerPieceIndex = 0;
-  var enemyPieceIndex = 0;
-
   for (var pieceNumber = 0; pieceNumber < 64; pieceNumber++) {
+    //Looping through each of the playable squares
+    //Create a local var to hold the playable square in question that the loop has brought focus to
     var innerBox = innerBoxes[pieceNumber];
     if (innerBox.className.includes("playableBox")) {
       //This is where you want to put a soldier
       if (parseInt(innerBox.id) < 40) {
         //This is on the far side of the board (enemy side), therefore we are inserting red player pieces
+        //Create the piece as a div
         var newPiece = document.createElement('div');
+        //Give the piece an id and class
         newPiece.id = "enemy" + String(pieceNumber);
         newPiece.className = "enemySoldier piece";
+        //Append it to the square
         innerBox.appendChild(newPiece);
-        enemyPositions[enemyPieceIndex] = Number(innerBox.id);
-        console.log(enemyPositions[enemyPieceIndex]);
-        enemyPieceIndex++;
+        //Push it to the array containing all the enemy pieces
+        enemyPositions[enemyPieceIndex].push(Number(innerBox.id));
       } else if (parseInt(innerBox.id) > 60) {
+        //Same thing as above, just for player pieces
         var newPiece = document.createElement('div');
         newPiece.id = "player" + String(pieceNumber);
         newPiece.className = "playerSoldier piece";
         innerBox.appendChild(newPiece);
-        playerPositions[playerPieceIndex] = Number(innerBox.id);
-        playerPieceIndex++;
+        playerPositions[playerPieceIndex].push(Number(innerBox.id));
       }
     }
   }
+  //Just a test object, will be commented out soon
   var newPiece = document.createElement('div');
   newPiece.className = "enemySoldier piece";
   document.getElementById('56').appendChild(newPiece);
@@ -39,17 +47,25 @@ function addPieces() {
   console.log(enemyPositions[13]);
 }
 
-function setupBoard() {
 
+//Function that creates the board
+function setupBoard() {
+  //recognise the board div as a variable
   var maindiv = document.getElementById('mainBoard');
 
+  //Iterate over all the squares
   for (var acrossSquares = 1; acrossSquares <= 8; acrossSquares++) {
     for (var downSquares = 1; downSquares <= 8; downSquares++) {
+      //Variable for the id
       var name = String(acrossSquares) + String(downSquares);
+      //Create the square div
       var newDiv = document.createElement('div');
+      //Give it an id and a class name
       newDiv.id = name;
       newDiv.className = "innerBoxes";
+      //Append it
       maindiv.appendChild(newDiv);
+      //If its an even number, give it white, if its odd give it black
       if ((acrossSquares + downSquares) % 2 == 0) {
         newDiv.style.backgroundColor = "#ffffcc";
       } else {
@@ -59,10 +75,12 @@ function setupBoard() {
     }
   }
 
+  //Add pieces to the board
   addPieces();
 
 }
 
+//This function is called when the game is started by the user. playerStarting is a bool that represents whether the game is to be started by the AI or the player
 function startGame(playerStarting) {
   if (playerStarting) {
     playerMove();
@@ -74,16 +92,20 @@ function startGame(playerStarting) {
 function playerMove() {
   //Check for mandatory moves
   for (var i = 0; i < playerPositions.length; i++) {
+    //prevent variable hoisting
     (function () {
       var isKing = 1;
       if (document.getElementById(String(playerPositions[i])).classList.contains("playerKing")) {
+        //if the piece is a king, then it can go backwards. thus isKing is made -1 so that the for loop will iterate over all 4 possible positions not just 2
         isKing = -1;
       }
       //var that holds valid moves FOR THIS PIECE - purpose is for it to be passed onclick
       var validMoves = [];
       var validPiece = document.getElementById(String(playerPositions[i])).firstChild;
       var hasMoves = false;
+      //Iterate forward and back
       for (var i1 = isKing; i1 < 2; i1 += 2) {
+        //Iterate left and right
         for (var i2 = 9; i2 < 12; i2 += 2) {
           var pieceToCapture = checkPosition(playerPositions[i], -i1 * i2);
           if(enemyPositions.indexOf(pieceToCapture) != -1) {
@@ -101,7 +123,9 @@ function playerMove() {
         }
       }
       if (hasMoves) {
+        //give the piece an onclick if it has moves.
         console.log(validMoves.length);
+        //pass an array showing the possible positions as well as the piece's id
         validPiece.addEventListener("click", function () {movePiece(validMoves, this)});
       }
 
@@ -120,19 +144,21 @@ function checkPosition(playerPos, change) {
     difference = -1;
   }
   if ((playerPosFirstNumber + difference) == newTotalFirstNumber && newTotal % 10 < 9) {
-    //Number is accetable
+    //Number is acceptable
     return newTotal;
   } else {
     return -1;
   }
 }
 
+//recieved when a valid piece is clicked - recieves the possible positions and the id
 function movePiece(squaresToHighlight, caller) {
   console.log(squaresToHighlight.length);
   for (var squares = 0; squares < squaresToHighlight.length; squares++) {
     (function () {
       var squareToChange = document.getElementById(String(squaresToHighlight[squares]));
       squareToChange.classList.add('landingSpace');
+      //onclick, pass the id of the piece that needs to be moved, as well as the square its going to move to
       squareToChange.setAttribute('onclick', 'commitMove(caller, squaresToHighlight[squares])');
     }());
   }
